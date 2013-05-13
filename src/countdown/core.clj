088a09ae2f4b8da67ae5b2e851f4d97d98af41ea
ujihisa <ujihisa@gmail.com>
2013-05-12 19:04:16 -0700

    tests parsing

--- src/countdown/core.clj
+++ src/countdown/core.clj
@@ -7,7 +7,7 @@
 (def built-in-formatter (clj-time.format/formatters :basic-date-time))
 
 (defn -main [& args]
-  #_(prn (clj-time.core/parse built-in-formatter ""))
+  (prn (clj-time.format/parse built-in-formatter "20130603T000000.000Z"))
   (prn (clj-time.format/unparse built-in-formatter (clj-time.local/local-now)))
   #_(let [room "computer_science" msg "test" bot-verifier "bf0942db651053ad522cbbcbf39cd22d062ca7e4"]
     (let [result (clj-http.client/post

5edbde31830d33e1ee5d546e5a080845b3ff05ac
ujihisa <ujihisa@gmail.com>
2013-05-12 19:01:50 -0700

    uses clj-time

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject countdown "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[clj-http "0.7.2"]
+                 [clj-time "0.5.0"]
                  [org.clojure/clojure "1.5.1"]]
   :main countdown.core)
--- src/countdown/core.clj
+++ src/countdown/core.clj
@@ -1,8 +1,15 @@
 (ns countdown.core
-  (:require clj-http.client))
+  (:require clj-http.client)
+  (:require clj-time.core)
+  (:require clj-time.format)
+  (:require clj-time.local))
+
+(def built-in-formatter (clj-time.format/formatters :basic-date-time))
 
 (defn -main [& args]
-  (let [room "computer_science" msg "test" bot-verifier "bf0942db651053ad522cbbcbf39cd22d062ca7e4"]
+  #_(prn (clj-time.core/parse built-in-formatter ""))
+  (prn (clj-time.format/unparse built-in-formatter (clj-time.local/local-now)))
+  #_(let [room "computer_science" msg "test" bot-verifier "bf0942db651053ad522cbbcbf39cd22d062ca7e4"]
     (let [result (clj-http.client/post
                    "http://lingr.com/api/room/say"
                    {:form-params

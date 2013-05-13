756d61470f802631105c16fd331cfc8236e1fc27
ujihisa <ujihisa@gmail.com>
2013-05-12 18:56:37 -0700

    sample bot-triggered event

--- src/countdown/core.clj
+++ src/countdown/core.clj
@@ -1,4 +1,14 @@
-(ns countdown.core)
+(ns countdown.core
+  (:require clj-http.client))
 
 (defn -main [& args]
+  (let [room "computer_science" msg "test" bot-verifier "bf0942db651053ad522cbbcbf39cd22d062ca7e4"]
+    (let [result (clj-http.client/post
+                   "http://lingr.com/api/room/say"
+                   {:form-params
+                    {:room room
+                     :bot 'countdown
+                     :text (str msg)
+                     :bot_verifier bot-verifier}})]
+      (prn (:body result))))
   (prn 'hello))
